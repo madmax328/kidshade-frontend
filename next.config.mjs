5690178ce87b7fc1d6d066424f2e58cb6a0adbb4
madmax328 <ricardo.paneo@gmail.com>
2025-11-22 18:17:02 +0100

    Deploy frontend

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
-import type { NextConfig } from "next";
+import path from "path";
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@/": path.resolve(__dirname, "src/"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@lib": path.resolve(__dirname, "src/lib")
+    };
+    return config;
   }
 };
 
